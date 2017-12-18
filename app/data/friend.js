dcce47d8578e8ed925843066318d1f49edd9514c
unknown <masonjterry@gmail.com>
2017-12-17 18:33:36 -0600

    added files to html, working on click function to get data from survey

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,3 +1,7 @@
-let friends = [{ name: "Fred", photo: "#", scrores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]}]
+let friends = [{ name: "Fred", photo: "#", scrores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]}];
 
-module.exports = friends;
+document.getElementById("button").addEventListener("click", function(event) {
+if (event) {
+  console.log("i am working");
+}
+});
--- server.js
+++ server.js
@@ -2,16 +2,32 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let path = require("path");
 let app = express();
-let friend = require("./app/data/friend.js")
+let fs = require("fs");
 let PORT = 8080;
-
-import * as htmlRoutes from "htmlRoutes.js";
-
-import * as apiRouting from "apiRouting.js";
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+// Home page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+  });
+
+// survey page
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+// a stop to make sure that anything that is typed in the url will make sure it goes to the home page
+app.get("/:stop?", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+// app.get("/api/friends", function(req, res) {
+//     fs.readFile("friend.js", "utf8", function(err, data) {
+//       return res.json(data);
+//     });
+// });
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT)
