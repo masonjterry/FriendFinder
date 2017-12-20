802d660060df4b0842a1d727c340dde87677ab68
unknown <masonjterry@gmail.com>
2017-12-19 23:31:57 -0600

    changed server.js body parser code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let htmlRoutes = require("./app/routing/htmlRoutes.js");
 let apiRoutes = require("./app/routing/apiRouting.js");
 let PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // html routes
