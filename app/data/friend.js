dc52a2f4ed2affd29cba5cd50f121e4a8ecf0ccc
unknown <masonjterry@gmail.com>
2017-12-20 20:53:10 -0600

    working on matching function

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,3 +1,3 @@
-let friends = [{ name: "Rick", photo: "http://images.tritondigitalcms.com/6616/sites/356/2017/07/28103713/Rick-Astley.jpg", scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]}, {name: "Tom", photo: "https://lovelace-media.imgix.net/uploads/259/da8e6e30-0a0e-0132-07c7-0eae5eefacd9.jpg?w=740&h=740&fit=crop&crop=faces&auto=format&q=70", scores: [3, 5, 4, 5, 3, 4, 3, 4, 4, 5]}, ];
+let friends = [{ name: "Rick", photo: "http://images.tritondigitalcms.com/6616/sites/356/2017/07/28103713/Rick-Astley.jpg", scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]}, {name: "Tom", photo: "https://lovelace-media.imgix.net/uploads/259/da8e6e30-0a0e-0132-07c7-0eae5eefacd9.jpg?w=740&h=740&fit=crop&crop=faces&auto=format&q=70", scores: ["3", "5", "4", "5", "3", "4", "3", "4", "4", "5"]}, ];
 
 module.exports = friends;
--- app/routing/apiRouting.js
+++ app/routing/apiRouting.js
@@ -8,13 +8,55 @@ module.exports = function apiRoutes(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    console.log("req.body", req.body);
+    //console.log('res.json', res.json());
     let newFriend = req.body;
     let scores = req.body.scores;
 
     console.log(newFriend);
 
+    // TODO: find best match
+    let bestMatch = {
+      name: "",
+      photo: "",
+      difference: Infinity
+    };
+
+    let currentDifference;
+
+    // loop through friends
+    for (let i = 0; i < friends.length; i++) {
+      let currentFriend = friends[i];
+    }
+
+    // loop through friends
+    friends.forEach(function(currentFriend) {
+
+      currentDifference = 0;
+
+      // loop over each score comparing currentFriend & newFriend
+      for (let i = 0; i < newFriend.scores.length; i++) {
+        let newFriendScore = newFriend.scores[i];
+        let currentFriendScore = currentFriend.scores[i];
+
+        // take the absolute value of the currentDifference
+        currentDifference += Math.abs(parseInt(newFriendScore) - parseInt(currentFriendScore));
+
+      }
+
+      // compare two scores & add difference to currentDifference
+      if (currentDifference < bestMatch.difference) {
+        bestMatch = {
+          name: currentFriend.name,
+          photo: currentFriend.photo,
+          difference: currentDifference
+        };
+      }
+
+    });
+
     friends.push(newFriend);
 
-    res.json(newFriend);
+    res.json(bestMatch);
   });
 }
